62fb4b3b0c0a10a8d3f294e53270c97f136da2cf
t5zeli00 <t5zeli00@students.oamk.fi>
2017-04-17 10:09:19 +0300

    Add language settings

--- main/java/com/easydrivesdp2/easy/SplashScreen.java
+++ main/java/com/easydrivesdp2/easy/SplashScreen.java
@@ -1,7 +1,16 @@
 package com.easydrivesdp2.easy;
         import android.app.Activity;
         import android.content.Intent;
+        import android.content.res.Configuration;
         import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.PopupMenu;
+        import android.widget.Toast;
+
+        import java.util.Locale;
 
 public class SplashScreen extends Activity {
 
@@ -26,11 +35,56 @@ public class SplashScreen extends Activity {
         timerThread.start();
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.lang, menu);
+        return true;
+    }
+
     @Override
     protected void onPause() {
         // TODO Auto-generated method stub
         super.onPause();
         finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        boolean change=false;
+
+        String languageToLoad;
+
+        switch (item.getItemId()) {
+            case R.id.en:
+                languageToLoad = "en";
+                Locale locale = new Locale(languageToLoad);
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config,
+                        getBaseContext().getResources().getDisplayMetrics());
+                this.setContentView(R.layout.activity_main);
+                change=true;
+                break;
+            case R.id.ru:
+                languageToLoad = "ru";
+                locale = new Locale(languageToLoad);
+                Locale.setDefault(locale);
+                config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config,
+                        getBaseContext().getResources().getDisplayMetrics());
+                this.setContentView(R.layout.activity_main);
+                change=true;
+                break;
+            default:
+                break;
+        }
+        if (change == true){
+            recreate();
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 
 }
